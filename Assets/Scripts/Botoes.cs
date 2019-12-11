09f452af967d3663cb9f9f8dfe9c17e1066412ff
Felipe V. Sobral <xfelipesobral@gmail.com>
2019-12-11 14:12:19 -0300

    init

---
+++ Assets/Scripts/Botoes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botoes : MonoBehaviour
+{
+    public GameObject camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(camera.transform.position);
+    }
+}
---
+++ Assets/Scripts/VerPlaneta.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class VerPlaneta : MonoBehaviour
+{
+    public void abrirPlaneta(string planetaNome)
+    {
+        SceneManager.LoadScene(planetaNome);
+    }
+
+    public void velocidade()
+    {
+        GameObject SliderGet;
+
+        SliderGet = GameObject.Find("Slider");
+        Time.timeScale = SliderGet.GetComponent<Slider>().value;
+    }
+}
+
